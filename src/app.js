12f671cdd551586240410bd564eaee0c21826ca4
Suzanna <skwong25@hotmail.co.uk>
2020-08-12 16:45:04 +0100

    began cat api codebase - all http requests working, as per manual testing on Postman. Automated testing to be implemented

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 // CAT API
-// allows CRUD functionality, databases sold seperately 
 
 const cats = require('./catsDb.js');
 const breeds = require('./breedsDb.js');
@@ -22,18 +21,20 @@ const verifyId = (req, res, next) => {
 }
 
 const checkObject = (req, res, next ) => {
-    // parse object to return JSON object attached to req.body 
-    // not sure yet if I have to parse or data is accesible via req.query... 
+    if (req.query) {
+        next()
+    } else {
+        res.status(404).send('invalid information provided')
+    }
 }
+
+let nextId = 5;
 
 // set up a port
 // app.use('/path', callback) to mount middleware functions (default called on request received) 
 const PORT = 4001;
 
 app.use(morgan('tiny'));
-
-console.log(`test: ${cats}`);
-console.log(`test: ${breeds}`);
 
 // GET request all 
 app.get('/cats', ( req, res, next ) => {
@@ -42,37 +43,38 @@ app.get('/cats', ( req, res, next ) => {
 
 // GET request by id 
 app.get('/cats/:id', verifyId, ( req, res, next ) => {
-        res.send(cats[req.catIndex]);
+    res.send(cats[req.catIndex]);
 });
 
 // POST request  
-app.post('/cats', ( req, res, next ) => {
-    if (req.query.name) {
-        const newCat = req.query;
-        const lastId = cats[-1]["id"];
-        newCat.id = lastId++;
-        cats.push(newCat);
-        const lastAdded = cats.slice(-1); // extracts last element to check object has added to the array
-        res.send(lastAdded); 
-    } else {
-        res.status(404).send('incomplete info')
-    }
+app.post('/cats', checkObject, ( req, res, next ) => {
+    const catNew = req.query;
+    catNew.id = nextId++;              // returns value of nextId, then increments by 1
+    cats.push(catNew);
+    const lastAdded = cats.slice(-1); // extracts last element to check object added to the array
+    res.send(lastAdded);  
 });
 
-// PUT request  
-app.put('/cats/:id', verifyId, ( req, res, next ) => {
-        if (req.query.name && req.query.descrip) { // write middleware function to extract object check 
-            const catUpdates = res.query; 
-            cats[req.catIndex] = catUpdates;
-        } else { 
-            res.status(404).send('incomplete info')
-        } 
+// PUT request - allows user to add/update information by id
+app.put('/cats/:id', verifyId, checkObject, ( req, res, next ) => {
+    const catUpdates = req.query;                  // catUpdates to be an object 
+    for (let key in catUpdates) {                  // loops to ensure each key-value pair is added 
+        let index = req.catIndex
+        console.log(`Index: ${index} , Key: ${key} `);
+        cats[index][key] = catUpdates[key]; 
+        console.log(cats[index]);
+        if (cats[index].hasOwnProperty(key)){      // check and reports back what has been updated 
+            console.log(`updated: ${key}: ${cats[index][key]}`);    
+        }
+    }
+    res.send(JSON.stringify(cats[req.catIndex]))    // converts JSON obj into JSON string
 });
 
 // DEL request 
-app.delete('/cats/:id', verifyId,  (req, res, next) => {
-        cats.splice(req.catIndex,1);
-        res.status(204).send('deletion successful');
+app.delete('/cats/:id', verifyId, (req, res, next) => {
+    console.log(`Index to be deleted: ${req.catIndex}`);
+    cats.splice([req.catIndex], 1);
+    res.status(204).send('deletion successful');
 });
 
 // set the server up (listening for reqs)
@@ -92,9 +94,12 @@ app.listen(PORT, () => {
 // -----------------------
 
 
-// EXTRAS: we can write error handlers 
-// separate cats by breed, try a nested router? 
-// separate out the indexChecker into a middleware function 
-// use morgan logger
-// do we need to use body-parser?
+// EXTRAS:
+// separate cats by breed or location, try a nested router? 
+// error handlers - not currently required 
+// do we need to use body-parser? NO - when do we need to use this? 
+// checks that can also be controlled in the UI:
+// breedId is a number 
+// coat is specific type
+// in PUT requests, if a value is the same, it doesn't need to get updated again
  
